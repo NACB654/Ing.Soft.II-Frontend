6b2c8cae1ea31ab8de5994cc7e6a64c83a9c1c7b
doliverosa <doa1602@gmail.com>
2024-05-16 20:44:18 -0500

    Avance pagina Home login, falta imagen

--- lumen.investiga-frontend/src/app/page.js
+++ lumen.investiga-frontend/src/app/page.js
@@ -3,7 +3,18 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      Contruir pantalla de login
+
+      <h1 className={styles.title}>LUMENINVESTIGA</h1>
+      <form className={styles.form}>
+        <label htmlFor="email">Correo</label>
+        <input type="email" id="email" className={styles.input} placeholder="Ingrese su correo" required />
+
+        <label htmlFor="password">Contraseña</label>
+        <input type="password" id="password" className={styles.input} placeholder="Ingrese su contraseña" required />
+
+        <button type="submit" className={styles.button}>Ingresar</button>
+        <button type="button" className={styles.buttonSecondary}>Crear Cuenta</button>
+      </form>    
     </main>
   );
 }
